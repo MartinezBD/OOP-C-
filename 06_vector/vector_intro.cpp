40a3d4fc7908e28cd52b26fae8a8c1cf054486cb
MartinezBD <bmartinezdothe2089@floridapoly.edu>
2026-09-11 15:28:26 +0000

    9/11 11:28am

--- 06_vector/vector_intro.cpp
+++ 06_vector/vector_intro.cpp
@@ -6,6 +6,9 @@ void print(const std::vector<int>& v);
 void print_backwards(const std::vector<int>& v);
 
 int main(void){
+
+    
+
     //create a vector
     std::vector<int> v; //empty vector (dynamic array) of integers
 
@@ -24,7 +27,39 @@ int main(void){
     std::vector<int> v4{v2};
     print(v4);
 
+    //get the size (number of elements)
+    std::cout << "v4 size:  " << v4.size() << std::endl;
 
+    //First and last element
+    std::cout << "v4 first:  " << v4.front() << std::endl;
+    std::cout << "v4 last:  " << v4.back() << std::endl;
+
+
+    //add the element at the end
+
+    v2.push_back(10);
+    v4.pop_back();
+    print(v4);
+
+
+    //iterator - object that points to a certain location in the vector
+    // begin() - points to the first element 
+    // end() - points to the last elememt
+
+    std::cout << *v2.begin() << std::endl;
+    std::cout << "here " << *(v2.begin() +3) << std::endl;
+
+    // Insert (location (interator) ,value)
+
+    v2.insert(v2.begin()+ 1, 999);
+    printf(v2);
+    v2.insert(v2.end() , 1000);
+    printf(v2);
+
+    //erase (location)
+
+    v2.erase(v2.begin());
+    printf(v2);
 
 
     return 0;
